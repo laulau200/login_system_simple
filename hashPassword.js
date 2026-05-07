d610f326b427601d4677053460a2e279981f6f9c
Laurent DEMAZY <146731191+laulau200@users.noreply.github.com>
2026-05-07 18:09:35 +0200

    Delete hashPassword.js

--- hashPassword.js
+++
@@ -1,9 +0,0 @@
-// utils/hashPassword.js
-const bcrypt = require('bcrypt');
-
-const hashPassword = async (password) => {
-  const saltRounds = 10;
-  return await bcrypt.hash(password, saltRounds);
-};
-
-module.exports = { hashPassword };
